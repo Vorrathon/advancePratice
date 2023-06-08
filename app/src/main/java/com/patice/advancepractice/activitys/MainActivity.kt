16047024e6a09e7b1f25602b9031211765050b5f
Vorrathon <109739489+Vorrathon@users.noreply.github.com>
2023-06-08 16:21:42 +0700

    first commit

---
+++ app/src/main/java/com/patice/advancepractice/activitys/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.patice.advancepractice.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.patice.advancepractice.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
